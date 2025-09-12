aeb191f4a3a42de637100d5e5778d3db05c0ee3e
Dhakshayini Usha R <dhakshayiniusha@gmail.com>
2025-09-12 20:56:20 +0530

    Create Public Mode of Inheritance.cpp

---
+++ Public Mode of Inheritance.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// Base class
+class Vehicle {
+  public: 
+    string brand = "Ford";
+    void color() {
+      cout << “Red! \n" ;
+    }
+};
+
+// Derived class
+class Car: public Vehicle {
+  public: 
+    string model = "Mustang";
+};
+
+int main() {
+  Car myCar;
+  myCar.color();
+  cout << myCar.brand + " " + myCar.model;
+  return 0;
+}
+
+/*Output:
+Red! 
+Ford Mustang
+*/
