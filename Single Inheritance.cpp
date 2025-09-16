9ec3795594b45788639934d663f73abb66cbfde9
Dhakshayini Usha R <dhakshayiniusha@gmail.com>
2025-09-16 17:37:41 +0530

    Create Single Inheritance.cpp

---
+++ Single Inheritance.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// Base class
+class Rectangle {
+public:
+    int width, height;
+
+    void read_input() {
+        cin >> width >> height;  // just input, no printing
+    }
+
+    void display() {
+        cout << width << " " << height << "\n";  // space separated
+    }
+};
+
+// Derived class (single inheritance)
+class RectangleArea : public Rectangle {
+public:
+    void display() {
+        int area = width * height;
+        cout << area << "\n";
+    }
+};
+
+int main() {
+    RectangleArea r_area;
+
+    // Read the width and height
+    r_area.read_input();
+
+    // Print width and height (using base class display)
+    r_area.Rectangle::display();
+
+    // Print area (using derived class display)
+    r_area.display();
+
+    return 0;
+}
