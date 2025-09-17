c7b76589ecfc83206f9b510d332ffc6b7477f309
Dhakshayini Usha R <dhakshayiniusha@gmail.com>
2025-09-17 16:02:18 +0530

    Update Hierarchical Inheritance.cpp

--- Hierarchical Inheritance.cpp
+++ Hierarchical Inheritance.cpp
@@ -6,52 +6,73 @@ ENTC A2
 #include <iostream>
 using namespace std;
 
-class Grandfather{
-    public :
+class Appliance {
+public: 
+    int price;
 
-    void  disp(){
-        cout<<"This is Grandfather class"<<"\n";
+    void setPrice() {
+        cout << "Enter Price: "<<"\n";
+        cin >> price;
+        cout << "Price : " << price << "\n";
+    }
+
+    void Purpose() {
+        cout << "The parent Class" << "\n";
     }
 };
 
-//Father class inherits from Grandfather class
-class Father : public Grandfather{
-    public :
-
-    void  disp(){
-        cout<<"This is Father class"<<"\n";
+class WashingMachine : public Appliance {
+public: 
+    void Purpose() {
+        cout << "Wash Clothes" << "\n";
     }
 };
 
-//Child class inherits from Father class : Hierarchical as it recieves methods and attributes from Grandfather also
-class Child : public Father{
-    public :
-
-    void  disp(){
-        cout<<"This is Child class"<<"\n";
+class Microwave : public Appliance {
+public: 
+    void Purpose() {
+        cout << "Cook Food" << "\n";
     }
 };
-int main(void){
-    Child c;
-    
-    cout<<"---Printing disp function of child class---"<<"\n";
-    c.disp();
-    cout<<"---Printing disp function of Father class---"<<"\n";
-    c.Father::disp();
-    cout<<"---Printing disp function of Grandfather class---"<<"\n";
-    c.Grandfather::disp();
-    
+
+int main() {
+    // Object of Washing Machine
+    WashingMachine w;
+    cout<<"---Calling setPrice of Parent Class Appliances---"<<"\n";
+    w.setPrice();
+    cout<<"---Calling Purpose of WashingMachine Class---"<<"\n";
+    w.Purpose();
+    cout<<"---Calling Purpose of Appliance Class---"<<"\n";
+    w.Appliance::Purpose();
+
+    // Object of Microwave
+    Microwave m;
+    cout<<"---Calling setPrice of Parent Class Appliances---"<<"\n";
+    m.setPrice();
+    cout<<"---Calling Purpose of Microwave Class---"<<"\n";
+    m.Purpose();
+    cout<<"---Calling Purpose of Appliance Class---"<<"\n";
+    m.Appliance::Purpose();
+
     return 0;
 }
 
 /*
 Sample Output: 
----Printing disp function of child class---
-This is Child class
----Printing disp function of Father class---
-This is Father class
----Printing disp function of Grandfather class---
-This is Grandfather class
+---Calling setPrice of Parent Class Appliances---
+Enter Price: 
+Price : 10000
+---Calling Purpose of WashingMachine Class---
+Wash Clothes
+---Calling Purpose of Appliance Class---
+The parent Class
+---Calling setPrice of Parent Class Appliances---
+Enter Price: 
+Price : 20000
+---Calling Purpose of Microwave Class---
+Cook Food
+---Calling Purpose of Appliance Class---
+The parent Class
 */
 
 
